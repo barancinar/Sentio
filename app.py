2ca22349c619944177ba799987dfda7756b79b73
barancinar <cinarbaran2003@gmail.com>
2025-02-22 15:30:10 +0300

    App Update

--- app.py
+++ app.py
@@ -249,9 +249,9 @@ def video_detect():
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 # if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))  # Render'ın atadığı portu kullan
-#     app.run(host="0.0.0.0", port=port, debug=True)
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render'ın atadığı portu kullan
+    app.run(host="0.0.0.0", port=port, debug=False)
