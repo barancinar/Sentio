2750af1c4e23cd9c97e054cb8da5e4436704b9ec
barancinar <cinarbaran2003@gmail.com>
2025-02-22 16:17:18 +0300

    Update Render

--- gunicorn_config.py
+++
@@ -1,6 +0,0 @@
-timeout = 120  # Timeout süresini artır
-workers = 2    # Worker sayısı
-threads = 2    # Thread sayısı
-worker_class = 'gthread'  # Thread bazlı worker
-max_requests = 1000      # Her worker'ın maksimum istek sayısı
-max_requests_jitter = 50 # Yeniden başlatma zamanlaması için jitter
